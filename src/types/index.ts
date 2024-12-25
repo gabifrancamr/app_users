e75d891889095ab65748665843b113905921ba26
Gabriela <gabifmouraroc@outlook.com>
2024-12-25 17:30:42 -0300

    arquivo de tipos

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: string
+  name: string
+  email: string
+  phone_number: string
+  photo_filename: string
+  created_at: string
+  is_admin: string
+  isBroker: string
+  Broker_name: string | null
+  Broker_phone: string | null
+  Broker_address: string | null
+  Broker_email: string | null
+}
+
+export interface TablePaginationProps {
+  currentPage: number
+  usersLength: number
+  totalPages: number
+}
