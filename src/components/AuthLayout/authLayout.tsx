be1da1a2314654a9db25431927ac285742935e0c
Gabriela <gabifmouraroc@outlook.com>
2024-12-27 16:07:58 -0300

    estilização auth

--- src/components/AuthLayout/authLayout.tsx
+++ src/components/AuthLayout/authLayout.tsx
@@ -3,7 +3,7 @@ import styles from './authLayout.module.css'
 
 export default function AuthLayout() {
   return (
-    <Flex flexDirection={"column"} justifyContent={"space-between"} backgroundColor={"#f5f5f5"} paddingY={"1rem"} paddingX={"2.5rem"} height={"100vh"}>
+    <Flex flexDirection={"column"} justifyContent={"space-between"} backgroundColor={"#f5f5f5"} paddingY={"1rem"} paddingX={"2.5rem"}>
       <span className={styles.title}>App</span>
       <footer className={styles.footer}>
         &copy; Desenvolvido por Gabriela França - {new Date().getFullYear()}
