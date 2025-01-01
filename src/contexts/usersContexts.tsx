4979c469d873360857728f0367dfe9a05523c721
Gabriela <gabifmouraroc@outlook.com>
2025-01-01 12:15:56 -0300

    alteração context api

--- src/contexts/usersContexts.tsx
+++ src/contexts/usersContexts.tsx
@@ -1,32 +1,15 @@
 "use client"
 
+import { User } from "@/types/types"
 import axios from "axios"
-import { ReadonlyURLSearchParams, useSearchParams } from "next/navigation"
 import { createContext, ReactNode, SetStateAction, useEffect, useState } from "react"
 
-export interface User {
-    id: string
-    name: string
-    email: string
-    phone_number: string
-    photo_filename: string
-    created_at: string
-    is_admin: string
-    isBroker: string
-    Broker_name: string | null
-    Broker_phone: string | null
-    Broker_address: string | null
-    Broker_email: string | null
-  }
 
 interface UsersContextType {
     users: User[]
     filteredUsers: User[]
     setFilteredUsers: (value: SetStateAction<User[]>) => void
     errorFindUsers: boolean
-    currentPage: number
-    searchParams: ReadonlyURLSearchParams
-    usersPerPage: number
 }
 
 export const UsersContext = createContext({} as UsersContextType)
@@ -39,11 +22,6 @@ export function UsersProvider({ children }: UsersProviderProps) {
     const [users, setUsers] = useState<User[]>([])
     const [filteredUsers, setFilteredUsers] = useState<User[]>([])
     const [errorFindUsers, setErrorFindUsers] = useState(false)
-    const searchParams = useSearchParams() 
-    const pageParam = searchParams?.get('page') || '1' 
-    
-    const currentPage = parseInt(pageParam, 10) || 1 
-    const usersPerPage = 5 
 
     useEffect(() => {
         async function fetchData() {
@@ -65,7 +43,7 @@ export function UsersProvider({ children }: UsersProviderProps) {
     return (
         <UsersContext.Provider
             value={{
-                users, filteredUsers, errorFindUsers, currentPage, usersPerPage, setFilteredUsers, searchParams
+                users, filteredUsers, errorFindUsers, setFilteredUsers
             }}
         >
             {children}
