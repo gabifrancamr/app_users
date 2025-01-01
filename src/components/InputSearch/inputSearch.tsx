30e64df94fc7100d34fc63ff103a87bd1dde2fa9
Gabriela <gabifmouraroc@outlook.com>
2025-01-01 12:16:49 -0300

    ajuste input search

--- src/components/InputSearch/inputSearch.tsx
+++ src/components/InputSearch/inputSearch.tsx
@@ -1,36 +1,35 @@
 "use client"
 
 import { UsersContext } from "@/contexts/usersContexts";
+import { useGetUsers } from "@/hooks/useGetUsers";
 import { Input } from "@chakra-ui/react";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
 export function InputSearch() {
-    const [inputSearch, setInputSearch] = useState('')
-    const { searchParams, users, setFilteredUsers } = useContext(UsersContext)
+    const searchParams = useSearchParams()
+    const searchTerm = searchParams?.get("search") || "";
+    const [search, setSearch] = useState(searchTerm)
+
+    const { users, setFilteredUsers } = useContext(UsersContext)
 
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    const inputSearchParam = searchParams?.get("search") || "";
-
-    useEffect(() => {
-        if (inputSearchParam) {
-            setInputSearch(inputSearchParam)
-        }
-    }, [])
 
     useEffect(() => {
         const params = new URLSearchParams(searchParams);
 
-        if (inputSearch.length >= 1) {
+        if (search.length >= 1) {
             params.set('page', '1');
-            params.set("search", inputSearch)
+            params.set("search", search)
+
+            const { data: filteredUsers } = useGetUsers(searchTerm);
 
             const filtered = users.filter(
                 (user) =>
-                    user.name.toLowerCase().includes(inputSearch.toLowerCase()) ||
-                    user.email.toLowerCase().includes(inputSearch.toLowerCase()),
+                    user.name.toLowerCase().includes(search.toLowerCase()) ||
+                    user.email.toLowerCase().includes(search.toLowerCase()),
             )
 
             setFilteredUsers(filtered)
@@ -40,12 +39,12 @@ export function InputSearch() {
         }
 
         replace(`${pathname}?${params.toString()}`);
-    }, [inputSearch, users])
+    }, [search, users])
 
     return (
         < Input
-            value={inputSearch}
-            onChange={(e) => setInputSearch(e.target.value)
+            value={search}
+            onChange={(e) => setSearch(e.target.value)
             }
         />
     )
